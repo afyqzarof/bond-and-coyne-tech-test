6285fc92c24d2cf4cb483ad5fff8717b5534a25f
afyqeyzar <norafyqe@gmail.com>
2025-02-20 17:50:26 +0800

    add modal component

---
+++ src/app/components/CharacterInfoModal.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CharacterInfoModal() {
+  return <div>CharacterInfoModal</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import { useEffect, useState } from 'react';
 import { Character } from './types';
 import { CharacterCard } from './components/CharacterCard';
+import CharacterInfoModal from './components/CharacterInfoModal';
 
 export default function Home() {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [showInfoModel, setShowInfoModel] = useState(false);
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -29,6 +31,10 @@ export default function Home() {
     fetchCharacters();
   }, []);
 
+  const handleMoreInfo = () => {
+    setShowInfoModel(true);
+  };
+
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
@@ -46,22 +52,24 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-16">
-      <h1 className="text-3xl font-bold mb-8 text-center">
-        Rick and Morty Characters
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {characters.map((character) => (
-          <CharacterCard
-            key={character.id}
-            character={character}
-            onMoreInfo={() => {
-              // Open Modal
-              console.log('More info clicked for:', character.name);
-            }}
-          />
-        ))}
+    <>
+      <div className="container mx-auto px-4 py-16">
+        <h1 className="text-3xl font-bold mb-8 text-center">
+          Rick and Morty Characters
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {characters.map((character) => (
+            <CharacterCard
+              key={character.id}
+              character={character}
+              onMoreInfo={() => {
+                handleMoreInfo();
+              }}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <CharacterInfoModal />
+    </>
   );
 }
